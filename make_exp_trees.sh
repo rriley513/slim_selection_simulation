6abdb6340c6ea8d0437ea03b6f75b0c59eb16a6d
Rebecca <rriley513@gmail.com>
2023-04-28 19:57:55 -0400

    add nohup capibilities to make_exp_trees

--- make_exp_trees.sh
+++ make_exp_trees.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-NUM_LOOPS=$1
+NUM_TREES=$1
 NUM_CONCURRENT=2 # constant
 
 readarray -d ',' -t RECOS < $2
-IFS=';' read -ra PARAMS <<< $3
+IFS=',' read -ra PARAMS <<< $3
 
 OUTFOLDER=$4
 PREFIX=$5
@@ -12,14 +12,14 @@ SLIM_FILE=$6
 SELECTION_STRENGTH=$7
 
 function run_slim(){
-    echo slim -d reco=${RECOS[i]} -d N1=${PARAMS[0]} -d N2=${PARAMS[1]} \
+    nohup slim -d reco=${RECOS[i]} -d N1=${PARAMS[0]} -d N2=${PARAMS[1]} \
         -d growth=${PARAMS[2]} -d T1=${PARAMS[3]} -d T2=${PARAMS[4]} \
-        -d output=\"$OUTFOLDER\" -d mut=$SELECTION_STRENGTH $SLIM_FILE \
-;}
+        -d outfolder=\"$OUTFOLDER\" -d mut=$SELECTION_STRENGTH $SLIM_FILE \
+        > $i.out & }
 
-for ((i=0; i<$NUM_LOOPS; i++))
+for (( i=0; i<$NUM_TREES; i++ ))
 do
-    for ((j=0; j<$NUM_CONCURRENT; j++))
+    for (( j=0; j<$NUM_CONCURRENT; j++ ))
     do
         run_slim
         i=$((i+1))
